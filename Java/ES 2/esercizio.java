08c36e67591e640b4fe74002ca732b98bcd0b5dd
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-10-01 16:48:46 +0200

    Commenti e modifiche

--- Java/ES 2/esercizio.java
+++ Java/ES 2/esercizio.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 /*----------------------------------------------------------------------------------------------------
 Eduardo Boanca 4C Informatica 1/10/2021
 ES 2: Effettuare l'input di un numero N che dica con quante persone si svolge l'input.
@@ -12,53 +11,54 @@ Opzionale : Dire quale persona è più vecchia/giovane.
 -----------------------------------------------------------------------------------------------------*/
 
 public class esercizio {
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader tastiera = new BufferedReader(input);
-              
+
         int n = 0, età = 0, min = 120, max = 0, err = 0;
         float media = 0;
-        
-        do
-        {
 
-        System.out.println("Salve, inserisca il numero di persone di cui si vuole fare la media");
-        try
-        {
-            String numeroLetto = tastiera.readLine();
-            n = Integer.valueOf(numeroLetto).intValue();
-        }
-        catch(Exception e)
-        {
-            System.out.println("\nNON HAI INSERITO UN NUMERO");
-            err=1;
-        }
-    }while(err==1);
+        do {
 
-        for(int i = 1; i <= n; i++)
-        {
-            do
+            System.out.print("Salve, inserisca il numero di persone di cui si vuole fare la media "); //ho tolto ln dal print per fare input su stessa riga
+            try 
             {
-            System.out.println("Persona " + i);
-            System.out.println("Età: ");
-        try
-        {
-            String numeroLetto = tastiera.readLine();
-            età = Integer.valueOf(numeroLetto).intValue();
-        }
-        catch(Exception e)
-        {
-            System.out.println("\nNON HAI INSERITO UN NUMERO");
-            err=1;
-        }
-    }while(err==1);
+                err = 0;
+                String numeroLetto = tastiera.readLine();
+                n = Integer.valueOf(numeroLetto).intValue();
 
+            } 
+            catch (Exception e) 
+            {
+                System.out.println("\nNON HAI INSERITO UN NUMERO");
+                err = 1;
+            }
+        } while (err == 1 || n < 2); //n deve essere maggiore di due perche non ha senso fare la media di una persona
 
-        media = media + età;
-        if(età < min) min = età;
-        if(età > max) max = età;
-        System.out.println("Media: " + media);
+        for (int i = 1; i <= n; i++) {
+
+            do {
+                System.out.println("Persona " + i);
+                System.out.print("Età: ");//ho tolto ln dal print per fare input su stessa riga
+                try 
+                {
+                    err = 0;
+                    String numeroLetto = tastiera.readLine();
+                    età = Integer.valueOf(numeroLetto).intValue();
+                    err = 0;
+                } 
+                catch (Exception e) 
+                {
+                    System.out.println("\nNON HAI INSERITO UN NUMERO");
+                    err = 1;
+                }
+            } while (err == 1);
+
+            media = media + età;
+            if (età < min)
+                min = età;
+            if (età > max)
+                max = età;
         }
 
         media = media / n;
@@ -66,6 +66,5 @@ public class esercizio {
         System.out.println("La persona più anziana ha l'età di " + max);
         System.out.println("La persona più giovane ha l'età di " + min);
 
-        
     }
 }
