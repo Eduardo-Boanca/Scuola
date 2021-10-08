f65ce3f3535b6f292ebe982438e4d7f606d017a1
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-10-08 14:21:32 +0200

    Modifiche file automobile

--- Java/Classi/Main.java
+++ Java/Classi/Main.java
@@ -1,15 +1,23 @@
-public class Main {
-    
-    public static void main(String[] args)
-    {
-        Automobile auto = new Automobile("DG512WF","BMW", 5000, 0, 0);
+public class Main
+{//inizio classe eseguibile
 
-        auto.presentati();
-        auto.accendi();
-        auto.acceleration(150);
-        auto.acceleration(50);
-        auto.presentati();
-        auto.spegni();
-        auto.presentati();
-    }
-}
+  public static void main(String args[])
+  {//inizio main
+
+    Automobile auto;// Dichiara un riferimento a un oggetto di classe automobile
+    auto=new Automobile("FB333IA","FERRARI",5000,0,0);//Crea l'oggetto auto di classe automobile
+    
+  //auto.targa = "IY567UT"; errore se l'attributo è       private
+    auto.presentati();
+    auto.setTarga ("IY567UT"); //da la possibilità di accedere al dato poichè il metodo è pubblico
+    auto.accendi();
+    auto.accelera(300);
+    auto.accelera(50);
+    //System.out.println("L'auto ha targa : " + auto.getTarga());
+    auto.presentati();
+    auto.spegni();
+    auto.presentati();
+    
+  }//fine main
+
+}//fine classe eseguibile
--- Java/Classi/automobile.java
+++ Java/Classi/automobile.java
@@ -1,59 +1,79 @@
-class Automobile {
-    //Attributi
-    String targa;
-    String marca;
-    int cavalli;
-    int stato; //stato di accesione 0 = spento, 1 = acceso;
-    int speed;
+public class Automobile
+{//inizio classe
+  
+  //attributi
+  private String targa; // aggiungendo private il programma si blocca
+  private String marca;
+  private int cavalli;
+  private int stato; //stato di accensione 0=spento 1=acceso
+  private int velocita;
 
-    //Costruttore
-    //costruttore con una firma che inizia con una stringa
-    public Automobile(String mytarga, String mymarca, int mycavalli, int mystato, int myspeed)
-    {
-        marca = mymarca;
-        targa = mytarga;
-        cavalli = mycavalli;
-        stato = mystato;
-        speed = myspeed;
-    }
+  //costruttore
+   public Automobile(String myTarga, String myMarca, int myCavalli, int myStato, int myVelocita)
+   {
+     this.marca=myMarca; // this = di questo oggetto
+     this.targa=myTarga;
+     this.cavalli=myCavalli;
+     this.stato=myStato;
+     this.velocita=myVelocita;
+   }
+    /*costruttore
+   public Automobile( int mystato, String mymarca, String mytarga , int mycavalli, int myvelocita)
+   {
+     marca=mymarca;
+     targa=mytarga;
+     cavalli=mycavalli;
+     stato=mystato;
+     velocita=myvelocita;
+   }
+  */
+  
+  public String getTarga()
+  {
+    return this.targa;
+  }
 
-    //un secondo costruttore con una firma diversa (prima un intero e poi una stringa)
-    public Automobile(int mycavalli, String mytarga, String mymarca, int mystato, int myspeed)
-    {
-        marca = mymarca;
-        targa = mytarga;
-        cavalli = mycavalli;
-        stato = mystato;
-        speed = myspeed;
-    }
+  public int getVelocita()
+  {
+    return this.velocita;
+  }
 
-    //Metodi
-    void accendi()
-    {
-        stato = 1;
-    }
+  public void setTarga(String myTarga) // Metodo che valorizza l'attributo targa in base al parametro passato in input
+  {
+    targa = myTarga;
+  }
 
-    void spegni()
-    {
-        stato = 0;
-        speed = 0;
-    }
+  public void setVelocita(int myVelocita) // Metodo che valorizza l'attributo velocita in base al parametro passato in input
+  {
+    velocita = myVelocita;
+  }
 
-    void presentati()
-    {
-        System.out.println("La mia targa è " + targa);
-        System.out.println("La mia marca è " + marca);
-        
-        if (stato == 0)
-            System.out.println("Sono spenta ");
-        else
-            System.out.println("Sono accesa ");
-        
-        System.out.println("La mia velocità è " +speed+ "km/h");
-    }
+  //metodi
+  void accendi()
+  {
+    stato=1;
+    velocita=0;
+  }
 
-    void acceleration(int incremento)
-    {
-        speed += incremento;
-    }
-}
+  void spegni()
+  {
+    stato=0;
+    velocita=0;
+  }
+
+  void accelera(int incremento)
+  {
+    velocita=velocita+incremento;
+  }
+
+  void presentati()
+  {
+    System.out.println("la mia targa e' "+this.getTarga());
+    System.out.println("la mia marca e' "+marca);if(stato==0)
+      System.out.println("io sono spenta");
+    else
+      System.out.println("io sono accesa");
+    System.out.println("la mia velocita e' "+this.getVelocita()+" km/h");
+    
+  }
+}//fine classe 
