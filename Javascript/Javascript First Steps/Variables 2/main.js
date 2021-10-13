c36f986449e185a50c3194e031b195deff189a3f
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2021-10-13 20:31:18 +0200

    Aggiunta Codice

---
+++ Javascript/Javascript First Steps/Variables 2/main.js
@@ -0,0 +1,11 @@
+
+// Add your code here
+
+let myName = 'Eduardo';
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+let para = document.createElement('p');
+para.textContent = myName;
+section.appendChild(para);
