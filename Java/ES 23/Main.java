52c1e082ac74494119e894694f48c475ddcf5ad9
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2022-05-06 16:18:03 +0200

    modifiche programma API

--- Java/ES 23/Main.java
+++ Java/ES 23/Main.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.nio.file.Paths;
 import java.io.*;
 import java.util.*;
 
@@ -6,22 +7,21 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://ergast.com/api/f1/current/last/results");
+            URL url = new URL("http://ergast.com/api/f1/current/last/results.xml");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
 
             int responseCode = conn.getResponseCode();
-            ArrayList<String> raceResults = new ArrayList<String>();
+            // ArrayList<String> raceResults = new ArrayList<String>();
 
             if (responseCode != 200) {
                 throw new RuntimeException("HttpResponseCode " + responseCode);
@@ -29,33 +29,49 @@ public class Main {
                 StringBuilder infoString = new StringBuilder();
                 Scanner scanner = new Scanner(url.openStream());
 
-                for (int i = 0; i < scanner.nextLine().length(); i++) {
-                    while (scanner.hasNext()) {
-                        infoString.append(scanner.nextLine());
-                        raceResults.add(infoString.toString());
-                    }
+                while (scanner.hasNext()) {
+                    infoString.append(scanner.nextLine());
                 }
 
+                // DATI SU FILE JSON CON JACKSON API
+                // ObjectMapper mapper = new ObjectMapper();
+                // try {
+                //     mapper.writeValue(Paths.get("Race_Results.json").toFile(), infoString);
+                // } catch (JsonProcessingException e) {
+                //     e.printStackTrace();
+                // }
+
+                // *****PER ESPORTARE UN FILE TXT*****//
                 try {
-                    // Classe responsabile per il processo di creazione del file XML da Oggetto
-                    JAXBContext jaxbContext = JAXBContext.newInstance(Main.class);
-                    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
-                    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-                    // Stampa il file nella console
-                    // jaxbMarshaller.marshal(infoString, System.out);
-
-                    // Crea il file nel PC
-                    FileOutputStream output = new FileOutputStream("Race_Results.xml");
-                    jaxbMarshaller.marshal(infoString, output);
-                } catch (JAXBException e) {
-                    e.printStackTrace();
+                PrintWriter out = new PrintWriter("Race_Results.txt");
+                out.println(infoString.toString());
+                } catch (FileNotFoundException e) {
+                System.err.println("File doesn't exist");
+                e.printStackTrace();
                 }
+
+
+                //PER ESPORTARE I DATI IN XML
+                // try {
+                // // Classe responsabile per il processo di creazione del file XML da Oggetto
+                // JAXBContext jaxbContext = JAXBContext.newInstance(Main.class);
+                // Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+                // jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+                // // Stampa il file nella console
+                // jaxbMarshaller.marshal(infoString, System.out);
+
+                // // Crea il file nel PC
+                // FileOutputStream output = new FileOutputStream("Race_Results.xml");
+                // jaxbMarshaller.marshal(infoString, output);
+                // } catch (JAXBException e) {
+                // e.printStackTrace();
+                // }
 
                 scanner.close();
 
-                // System.out.println(infoString);
+                //System.out.println(infoString);
 
             }
 
