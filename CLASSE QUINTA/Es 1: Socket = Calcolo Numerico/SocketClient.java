60bd91ad9496285f3b2d7c3a9b713c2469dd4539
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2022-09-19 19:43:17 +0200

    modifiche e fine programma

--- CLASSE QUINTA/Es 1: Socket = Calcolo Numerico/SocketClient.java
+++ CLASSE QUINTA/Es 1: Socket = Calcolo Numerico/SocketClient.java
@@ -1,77 +1,28 @@
-import java.net.*;
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SocketClient {
-
-    private final static int PORTA_SERVER = 9999;
-    private final static String NOME_SERVER = "127.0.0.1";
-    private int numeroUtente;
-    private int numeroServer;
-    private static Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-
-    public static void main(String[] args) throws IOException {
-        try {
-            clientSocket = new Socket(NOME_SERVER, PORTA_SERVER);
-        } catch (UnknownHostException e) {
-            System.out.println("Host sconosciuto " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Errore di comuncazione " + e.getMessage());
+    public static void main (String[] args)
+    {
+        int numero = 10;
+        
+        //creare la connessione con il server
+        try{
+            Socket clientSocket = new Socket("localhost",9999);
+            System.out.println("Il numero è: " + numero);
+            //inviare il numero 
+            clientSocket.getOutputStream().write(numero);
+            if(clientSocket.getInputStream().read() == 0) {
+                System.out.println("Il Numero " + numero + " è divisibile per 7");
+            } else {
+                System.out.println("Il Numero " + numero + " non è divisibile per 7");
+            }
+            } catch (UnknownHostException e){
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-        accessoServer();
-    }
-
-
-    public static void accessoServer() {
-        try {
-            Scanner input = new Scanner(clientSocket.getInputStream());
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            //Scanner inputUtente = new Scanner(System.in);
-
-            int numeroInserito, rispostaServer;
-
-            do {
-                System.out.println("Inserisci il numero ");
-                numeroInserito = ((int) Math.random()*100);
-                //numeroInserito = inputUtente.nextInt();
-
-                //invio numeri al server
-                output.println(numeroInserito);
-                rispostaServer = input.readLine(); //ricevo risposta dal server
-                System.out.println("SERVER: " + rispostaServer);
-            } while(numeroInserito != 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            clientSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void connetti() {
-        System.out.println("Client: creazione della connessione");
-    }
-
-    public void comunica() {
-        try {
-            numeroUtente = ((int) (Math.random()*70));
-            System.out.println("numero utente: " + numeroUtente);
-            System.out.println("Client: invio del numero al server");
-            out.println(numeroUtente);
-            numeroServer = in.nextInt();
-            System.out.println("Client: Risposta del server: " + numeroServer);
-            System.out.println("Chiusura connessione");
-            clientSocket.close();
-        } catch (Exception e) {
-            System.out.println("Errore di comunicazione");
-        }
-    }
-
-
 }
--- CLASSE QUINTA/Es 1: Socket = Calcolo Numerico/SocketServer.java
+++ CLASSE QUINTA/Es 1: Socket = Calcolo Numerico/SocketServer.java
@@ -1,46 +1,26 @@
-import java.net.*;
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class SocketServer {
-
-    private final static int PORTA_SERVER = 9999;
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-
-    public static void main(String[] args) {
-        try {
-            serverSocket = new ServerSocket(PORTA_SERVER);
-        } catch (Exception e) {
-            System.err.println("Errore interno del server " + e.getMessage());
-        }
-
-        gestisciClient();
-    }
-
-    public static void gestisciClient() {
-        try {
-            clientSocket = serverSocket.accept();
-            Scanner input = new Scanner(clientSocket.getInputStream());
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
-            int numeroRicevuto = input.nextInt(); // numero ricevuto dal client
-            int risposta;
-
-            if (numeroRicevuto % 7 == 0) {
-                risposta = numeroRicevuto;
-                output.println(risposta);
-            } else {
-                risposta = numeroRicevuto;
-                output.println(risposta);
+public class SocketServer{
+    public static void main (String[] args)
+    {
+        //creare il server
+        try{
+            ServerSocket serverSocket = new ServerSocket(9999);
+            while (true)
+            {
+                //accettare connessioni dal cliente 
+                Socket clientSocket = serverSocket.accept();
+                //leggere i dati passati 
+                int numero = clientSocket.getInputStream().read();
+                numero = numero % 7;
+                clientSocket.getOutputStream().write(numero);
+                //tornar eall'inizio per accettare una nuova connessione
+                clientSocket.close();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-        try {
-            clientSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
