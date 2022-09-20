9ea0d9929a62d09c262edd578e89b9617ffe8444
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2022-09-20 20:11:14 +0200

    modifiche

--- CLASSE QUINTA/Es 1: Socket = Calcolo Numerico/Server.java
+++ CLASSE QUINTA/Es 1: Socket = Calcolo Numerico/Server.java
@@ -2,25 +2,23 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class SocketServer{
-    public static void main (String[] args)
-    {
-        try{
-            //creazione del server
+public class Server {
+    public static void main(String[] args) {
+        try {
+            // creazione del server
             ServerSocket serverSocket = new ServerSocket(9999);
-            while (true)
-            {
-                //accettare connessioni dal cliente 
+            while (true) {
+                // accettare connessioni dal cliente
                 Socket clientSocket = serverSocket.accept();
-                //leggere i dati passati 
+                // leggere i dati passati
                 int numero = clientSocket.getInputStream().read();
                 numero = numero % 7;
                 clientSocket.getOutputStream().write(numero);
-                //chiusura della connessione
+                // chiusura della connessione
                 clientSocket.close();
             }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 }
